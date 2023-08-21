9dce17651f643d79f63d3edaf68f015055762390
Braulio Truylio <braulio.truylio@gmail.com>
2023-08-21 18:27:39 -0300

    create projeto 1 python
    Projeto exercício em python

---
+++ projeto1.py
@@ -0,0 +1,15 @@
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+print("Fim")
+
+print(" ")
+print("------------- ")
+print(" ")
+
+andar = 20
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
+print("Fim")
