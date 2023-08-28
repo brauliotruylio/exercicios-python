0bd8f3af864150d59097f1528d8f0261e0141183
Braulio Truylio <braulio.truylio@gmail.com>
2023-08-28 10:53:40 -0300

    update

--- odds.py
+++ odds.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import time
+import random
 
 odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
@@ -6,7 +8,8 @@ odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
 
 right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("Este minuto parece um pouco estranho.")
-else:
-    print("Não é um minuto estranho.")
+for i in range(0, 5, 1):
+    if right_this_minute in odds:
+        print("Este minuto parece um pouco estranho.")
+    else:
+        print("Não é um minuto estranho.")
